d308b8c425026c8dd7847e97744cbf6b424ee2b9
cpesar <cpesar1@gmail.com>
2022-02-05 20:10:03 -0700

    successful merge

--- src/App.js
+++ src/App.js
@@ -1,28 +1,25 @@
-import React, { useState } from 'react';
-import Nav from './components/Nav';
-import About from './components/About';
-import Gallery from './components/Gallery';
+import React, { useState } from "react";
+import Nav from "./components/Nav";
+import About from "./components/About";
+import Gallery from "./components/Gallery";
 // import PhotoList from './components/PhotoList';
 
-import ContactForm from './components/Contact';
+import ContactForm from "./components/Contact";
 
 function App() {
-
   const [contactSelected, setContactSelected] = useState(false);
 
   const [categories] = useState([
     {
       name: "commercial",
-      description: "Photos of grocery stores, food trucks, and other commercial projects",
+      description:
+        "Photos of grocery stores, food trucks, and other commercial projects",
     },
-    { name: "portraits", 
-      description: "Portraits of people in my life" 
-    },
-    { name: "food", 
-      description: "Delicious food delicacies" 
-    },
-    { name: "landscape", 
-      description: "Fields, farmhouses, waterfalls, and the beauty of nature" 
+    { name: "portraits", description: "Portraits of people in my life" },
+    { name: "food", description: "Delicious food delicacies" },
+    {
+      name: "landscape",
+      description: "Fields, farmhouses, waterfalls, and the beauty of nature",
     },
   ]);
 
@@ -30,21 +27,21 @@ function App() {
   return (
     <div>
       <Nav
-      categories={categories}
-      setCurrentCategory={setCurrentCategory}
-      currentCategory={currentCategory}
-      contactSelected={contactSelected}
-      setContactSelected={setContactSelected}
+        categories={categories}
+        setCurrentCategory={setCurrentCategory}
+        currentCategory={currentCategory}
+        contactSelected={contactSelected}
+        setContactSelected={setContactSelected}
       ></Nav>
       <main>
-       {!contactSelected ? (
-         <>
-          <Gallery currentCategory={currentCategory}></Gallery>
-          <About></About>
-         </>
-       ) : (
+        {!contactSelected ? (
+          <>
+            <Gallery currentCategory={currentCategory}></Gallery>
+            <About></About>
+          </>
+        ) : (
           <ContactForm></ContactForm>
-       )}
+        )}
       </main>
     </div>
   );
